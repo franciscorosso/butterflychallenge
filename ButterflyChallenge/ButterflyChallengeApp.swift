e395352d6397ecfad271ec0d4ee744afeb160d57
Francisco Rosso <franciscorosso@icloud.com>
2025-11-24 08:40:50 -0300

    Initial commit

---
+++ ButterflyChallenge/ButterflyChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButterflyChallengeApp.swift
+//  ButterflyChallenge
+//
+//  Created by Francisco Rosso on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ButterflyChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
